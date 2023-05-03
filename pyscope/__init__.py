0b45e7d06c207e7323d3dca5f94d1b9e59231e81
WGolay <wgolay30@gmail.com>
2023-05-03 16:14:09 -0500

    Update the highlevel init

--- pyscope/__init__.py
+++ pyscope/__init__.py
@@ -15,3 +15,40 @@ from .safety_monitor import SafetyMonitor
 from .switch import Switch
 from .telescope import Telescope
 from .wcs import WCS
+
+## Drivers
+# Abstract
+from drivers.abstract.autofocus import Autofocus
+from drivers.abstract.camera import Camera
+from drivers.abstract.cover_calibrator import CoverCalibrator
+from drivers.abstract.dome import Dome
+from drivers.abstract.driver import Driver
+from drivers.abstract.filter_wheel import FilterWheel
+from drivers.abstract.focuser import Focuser
+from drivers.abstract.observing_conditions import ObservingConditions
+from drivers.abstract.rotator import Rotator
+from drivers.abstract.safety_monitor import SafetyMonitor
+from drivers.abstract.switch import Switch
+from drivers.abstract.telescope import Telescope
+from drivers.abstract.wcs import WCS
+
+# ASCOM
+from drivers._ascom.camera import Camera
+from drivers._ascom.cover_calibrator import CoverCalibrator
+from drivers._ascom.dome import Dome
+from drivers._ascom.driver import Driver
+from drivers._ascom.filter_wheel import FilterWheel
+from drivers._ascom.focuser import Focuser
+from drivers._ascom.observing_conditions import ObservingConditions
+from drivers._ascom.rotator import Rotator
+from drivers._ascom.safety_monitor import SafetyMonitor
+from drivers._ascom.switch import Switch
+from drivers._ascom.telescope import Telescope
+
+# Custom
+from drivers.autofocus_pwi import Autofocus
+from drivers.maxim import Maxim
+from drivers.observatory_conditions_winer import ObservatoryConditions
+from drivers.safety_monitor_winerroof import SafetyMonitor
+from drivers.wcs_astrometrynet import WCS
+from drivers.wcs_pinpoint import WCS
