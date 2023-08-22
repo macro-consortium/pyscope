115171ecd23b92048ada387e36c796eddf65cf18
WGolay <wgolay30@gmail.com>
2023-08-21 22:06:37 -0400

    Add exoplanet_transits

--- pyscope/telrun/__init__.py
+++ pyscope/telrun/__init__.py
@@ -9,6 +9,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from .telrun_exception import TelrunException
+from .exoplanet_transits import exoplanet_transits
 from .init_dirs import init_telrun_dir, init_remote_dir
 from .schedtel import schedtel, plot_schedule_gantt, parse_sch_file
 from .startup import start_telrun, start_syncfiles
@@ -16,6 +17,7 @@ from .syncfiles import syncfiles
 from .telrun_operator import TelrunOperator
 
 __all__ = [
+    "exoplanet_transits",
     "init_telrun_dir",
     "init_remote_dir",
     "schedtel",
