11d2ff798de0060c7c92b75774d23b0f61506832
WGolay <wgolay30@gmail.com>
2023-08-22 19:58:36 -0400

    black run

--- pyscope/telrun/rst.py
+++ pyscope/telrun/rst.py
@@ -67,7 +67,7 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
     times are calculated for the observatory. If no date is given, then the current
     date at the location of the observatory is used. The source is first resolved
     by `astropy.coordinates.SkyCoord.from_name` and if not found, the `astroquery.mpc.MPCClass`
-    is used. Finally, if the source is not found, then `astropy.coordinates.get_body` is used 
+    is used. Finally, if the source is not found, then `astropy.coordinates.get_body` is used
     for solar system bodies. If the source is still not found, then the program exits with an
     error message.
 
@@ -174,7 +174,9 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
             )
             name = config.get("site", "name", fallback="pyscope observatory")
         else:
-            raise click.BadParameter("Observatory configuration file does not exist: %s" % observatory)
+            raise click.BadParameter(
+                "Observatory configuration file does not exist: %s" % observatory
+            )
     elif type(observatory) is Observatory:
         lat = observatory.observatory_location.lat
         lon = observatory.observatory_location.lon
@@ -196,8 +198,10 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
         date = datetime.datetime.strptime(date, "%Y-%m-%d")
     date = datetime.datetime(date.year, date.month, date.day, 12, 0, 0, tzinfo=tz)
 
-    t0 = astrotime.Time(datetime.datetime(date.year, date.month, date.day, 12, 0, 0, tzinfo=tz),
-        format='datetime')
+    t0 = astrotime.Time(
+        datetime.datetime(date.year, date.month, date.day, 12, 0, 0, tzinfo=tz),
+        format="datetime",
+    )
     logger.debug(f"t0 = {t0}")
 
     observer = astroplan.Observer(
@@ -205,25 +209,50 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
     )
 
     horizon = (
-        ('Sunset', observer.sun_set_time(t0, which="nearest")),
-        ('Sunrise', observer.sun_rise_time(t0, which="next")),
+        ("Sunset", observer.sun_set_time(t0, which="nearest")),
+        ("Sunrise", observer.sun_rise_time(t0, which="next")),
     )
     civil_twilight = (
-       ('Civil Twilight Start', observer.sun_set_time(t0, which="nearest", horizon=-6 * u.deg)),
-        ('Civil Twilight End', observer.sun_rise_time(t0, which="next", horizon=-6 * u.deg)),
+        (
+            "Civil Twilight Start",
+            observer.sun_set_time(t0, which="nearest", horizon=-6 * u.deg),
+        ),
+        (
+            "Civil Twilight End",
+            observer.sun_rise_time(t0, which="next", horizon=-6 * u.deg),
+        ),
     )
     nautical_twilight = (
-        ('Nautical Twilight Start', observer.sun_set_time(t0, which="nearest", horizon=-12 * u.deg)),
-        ('Nautical Twilight End', observer.sun_rise_time(t0, which="next", horizon=-12 * u.deg)),
+        (
+            "Nautical Twilight Start",
+            observer.sun_set_time(t0, which="nearest", horizon=-12 * u.deg),
+        ),
+        (
+            "Nautical Twilight End",
+            observer.sun_rise_time(t0, which="next", horizon=-12 * u.deg),
+        ),
     )
     astronomical_twilight = (
-        ('Astronomical Twilight Start', observer.sun_set_time(t0, which="nearest", horizon=-18 * u.deg)),
-        ('Astronomical Twilight End', observer.sun_rise_time(t0, which="next", horizon=-18 * u.deg)),
+        (
+            "Astronomical Twilight Start",
+            observer.sun_set_time(t0, which="nearest", horizon=-18 * u.deg),
+        ),
+        (
+            "Astronomical Twilight End",
+            observer.sun_rise_time(t0, which="next", horizon=-18 * u.deg),
+        ),
     )
 
-    times = [horizon[0], horizon[1], civil_twilight[0], civil_twilight[1],
-                nautical_twilight[0], nautical_twilight[1], astronomical_twilight[0],
-                astronomical_twilight[1]]
+    times = [
+        horizon[0],
+        horizon[1],
+        civil_twilight[0],
+        civil_twilight[1],
+        nautical_twilight[0],
+        nautical_twilight[1],
+        astronomical_twilight[0],
+        astronomical_twilight[1],
+    ]
 
     if source is not None:
         try:
@@ -264,9 +293,15 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
                     return
         logger.debug(f"obj = {obj}")
 
-        rise_time = ('%s Rise' % source, observer.target_rise_time(t0, obj, which="next"))
-        transit_time = ('%s Transit' % source, observer.target_meridian_transit_time(t0, obj, which="next"))
-        set_time = ('%s Set' % source, observer.target_set_time(t0, obj, which="next"))
+        rise_time = (
+            "%s Rise" % source,
+            observer.target_rise_time(t0, obj, which="next"),
+        )
+        transit_time = (
+            "%s Transit" % source,
+            observer.target_meridian_transit_time(t0, obj, which="next"),
+        )
+        set_time = ("%s Set" % source, observer.target_set_time(t0, obj, which="next"))
 
         logger.debug(f"rise_time = {rise_time}")
         logger.debug(f"transit_time = {transit_time}")
@@ -280,13 +315,15 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
             times.append(transit_time)
         if not isinstance(set_time[1].value, np.ma.MaskedArray):
             times.append(set_time)
-        
-        if isinstance(rise_time[1].value, np.ma.MaskedArray) and isinstance(set_time[1].value, np.ma.MaskedArray):
+
+        if isinstance(rise_time[1].value, np.ma.MaskedArray) and isinstance(
+            set_time[1].value, np.ma.MaskedArray
+        ):
             if isinstance(transit_time[1].value, np.ma.MaskedArray):
                 logger.warning("This source never rises above the horizon.")
             else:
                 logger.warning("This source is circumpolar.")
-        
+
     times.sort(key=lambda x: x[1].jd)
 
     tbl = prettytable.PrettyTable()
@@ -294,16 +331,24 @@ def rst_cli(source=None, date=None, observatory="./config/observatory.cfg", verb
     tbl.align["Time"] = "r"
 
     for t in times:
-        tbl.add_row([
-            t[0],
-            t[1].to_datetime(timezone=tz).strftime("%H:%M"),
-            t[1].strftime("%H:%M"),
-            observer.local_sidereal_time(t[1]).to_string(precision=0, sep=":", fields=2)])
+        tbl.add_row(
+            [
+                t[0],
+                t[1].to_datetime(timezone=tz).strftime("%H:%M"),
+                t[1].strftime("%H:%M"),
+                observer.local_sidereal_time(t[1]).to_string(
+                    precision=0, sep=":", fields=2
+                ),
+            ]
+        )
 
     if verbose > -1:
-        click.echo(f"Times from: {date.strftime('%Y-%m-%d %H:%M (%Z)')}, {t0.strftime('%Y-%m-%d %H:%M (UTC)')}")
+        click.echo(
+            f"Times from: {date.strftime('%Y-%m-%d %H:%M (%Z)')}, {t0.strftime('%Y-%m-%d %H:%M (UTC)')}"
+        )
         click.echo(tbl)
 
     return times
+
 
 rst = rst_cli.callback
