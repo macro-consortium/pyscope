765edee85ad68878dff71565295bedacdfc7044f
WGolay <wgolay30@gmail.com>
2023-08-31 14:58:33 -0400

    Add test_ascom_switch

--- pyscope/observatory/ascom_switch.py
+++ pyscope/observatory/ascom_switch.py
@@ -18,49 +18,49 @@ class ASCOMSwitch(ASCOMDevice, Switch):
 
     def CanWrite(self, ID):
         logger.debug(f"ASCOMSwitch.CanWrite({ID}) called")
-        return self._com_object.CanWrite(ID)
+        return self._device.CanWrite(ID)
 
     def Choose(self, SwitchID):
         logger.debug(f"ASCOMSwitch.Choose({SwitchID}) called")
-        self._com_object.Choose(SwitchID)
+        self._device.Choose(SwitchID)
 
     def GetSwitch(self, ID):
         logger.debug(f"ASCOMSwitch.GetSwitch({ID}) called")
-        return self._com_object.GetSwitch(ID)
+        return self._device.GetSwitch(ID)
 
     def GetSwitchDescription(self, ID):
         logger.debug(f"ASCOMSwitch.GetSwitchDescription({ID}) called")
-        return self._com_object.GetSwitchDescription(ID)
+        return self._device.GetSwitchDescription(ID)
 
     def GetSwitchName(self, ID):
         logger.debug(f"ASCOMSwitch.GetSwitchName({ID}) called")
-        return self._com_object.GetSwitchName(ID)
+        return self._device.GetSwitchName(ID)
 
     def MaxSwitchValue(self, ID):
         logger.debug(f"ASCOMSwitch.MaxSwitchValue({ID}) called")
-        return self._com_object.MaxSwitchValue(ID)
+        return self._device.MaxSwitchValue(ID)
 
     def MinSwitchValue(self, ID):
         logger.debug(f"ASCOMSwitch.MinSwitchValue({ID}) called")
-        return self._com_object.MinSwitchValue(ID)
+        return self._device.MinSwitchValue(ID)
 
     def SetSwitch(self, ID, State):
         logger.debug(f"ASCOMSwitch.SetSwitch({ID}, {State}) called")
-        self._com_object.SetSwitch(ID, State)
+        self._device.SetSwitch(ID, State)
 
     def SetSwitchName(self, ID, Name):
         logger.debug(f"ASCOMSwitch.SetSwitchName({ID}, {Name}) called")
-        self._com_object.SetSwitchName(ID, Name)
+        self._device.SetSwitchName(ID, Name)
 
     def SetSwitchValue(self, ID, Value):
         logger.debug(f"ASCOMSwitch.SetSwitchValue({ID}, {Value}) called")
-        self._com_object.SetSwitchValue(ID, Value)
+        self._device.SetSwitchValue(ID, Value)
 
     def SwitchStep(self, ID):
         logger.debug(f"ASCOMSwitch.SwitchStep({ID}) called")
-        self._com_object.SwitchStep(ID)
+        self._device.SwitchStep(ID)
 
     @property
     def MaxSwitch(self):
         logger.debug("ASCOMSwitch.MaxSwitch property called")
-        return self._com_object.MaxSwitch
+        return self._device.MaxSwitch
