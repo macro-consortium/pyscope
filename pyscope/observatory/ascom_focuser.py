14bb81783aca530187e53fb33a2220ac0f3dad62
WGolay <wgolay30@gmail.com>
2023-08-31 14:08:51 -0400

    Add test_ascom_focuser

--- pyscope/observatory/ascom_focuser.py
+++ pyscope/observatory/ascom_focuser.py
@@ -18,72 +18,72 @@ class ASCOMFocuser(ASCOMDevice, Focuser):
 
     def Choose(self, FocuserID):
         logger.debug(f"ASCOMFocuser.Choose({FocuserID}) called")
-        self._com_object.Choose(FocuserID)
+        self._device.Choose(FocuserID)
 
     def Halt(self):
         logger.debug(f"ASCOMFocuser.Halt() called")
-        self._com_object.Halt()
+        self._device.Halt()
 
     def Move(self, Position):
         logger.debug(f"ASCOMFocuser.Move({Position}) called")
-        self._com_object.Move(Position)
+        self._device.Move(Position)
 
     @property
     def Absolute(self):
         logger.debug(f"ASCOMFocuser.Absolute property called")
-        return self._com_object.Absolute
+        return self._device.Absolute
 
     @property
     def IsMoving(self):
         logger.debug(f"ASCOMFocuser.IsMoving property called")
-        return self._com_object.IsMoving
+        return self._device.IsMoving
 
     @property
     def Link(self):
         logger.debug(f"ASCOMFocuser.Link property called")
-        return self._com_object.Link
+        return self._device.Link
 
     @Link.setter
     def Link(self, value):
         logger.debug(f"ASCOMFocuser.Link property set to {value}")
-        self._com_object.Link = value
+        self._device.Link = value
 
     @property
     def MaxIncrement(self):
         logger.debug(f"ASCOMFocuser.MaxIncrement property called")
-        return self._com_object.MaxIncrement
+        return self._device.MaxIncrement
 
     @property
     def MaxStep(self):
         logger.debug(f"ASCOMFocuser.MaxStep property called")
-        return self._com_object.MaxStep
+        return self._device.MaxStep
 
     @property
     def Position(self):
         logger.debug(f"ASCOMFocuser.Position property called")
-        return self._com_object.Position
+        return self._device.Position
 
     @property
     def StepSize(self):
         logger.debug(f"ASCOMFocuser.StepSize property called")
-        return self._com_object.StepSize
+        return self._device.StepSize
 
     @property
     def TempComp(self):
         logger.debug(f"ASCOMFocuser.TempComp property called")
-        return self._com_object.TempComp
+        return self._device.TempComp
 
     @TempComp.setter
     def TempComp(self, value):
         logger.debug(f"ASCOMFocuser.TempComp property set to {value}")
-        self._com_object.TempComp = value
+        self._device.TempComp = value
 
     @property
     def TempCompAvailable(self):
         logger.debug(f"ASCOMFocuser.TempCompAvailable property called")
-        return self._com_object.TempCompAvailable
+        return self._device.TempCompAvailable
 
     @property
     def Temperature(self):
         logger.debug(f"ASCOMFocuser.Temperature property called")
-        return self._com_object.Temperature
+        return self._device.Temperature
---
+++ tests/observatory/test_ascom_focuser.py
@@ -0,0 +1,42 @@
+import time
+
+import pytest
+
+
+def test_move(device, disconnect):
+    device.Move(50)
+    while device.IsMoving:
+        time.sleep(0.1)
+    if device.Absolute:
+        assert device.Position == 500
+
+
+def test_halt(device, disconnect):
+    device.Move(50)
+    time.sleep(0.1)
+    device.Halt()
+
+
+def test_maxincrement(device, disconnect):
+    assert device.MaxIncrement is not None
+
+
+def test_maxstep(device, disconnect):
+    assert device.MaxStep is not None
+
+
+def test_stepsize(device, disconnect):
+    assert device.StepSize is not None
+
+
+def test_tempcomp(device, disconnect):
+    if device.TempCompAvailable:
+        assert device.TempComp is not None
+        device.TempComp = True
+        assert device.TempComp is True
+        device.TempComp = False
+        assert device.TempComp is False
+
+
+def test_temperature(device, disconnect):
+    assert device.Temperature is not None
