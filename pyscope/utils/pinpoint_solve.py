c29d90b329b8e9a9f61dc5c1967ee3e2f5b02b78
pgriffin17 <31374077+pgriffin17@users.noreply.github.com>
2024-01-11 19:56:27 -0600

    Tested with MaxIm, added logging to console

--- pyscope/utils/pinpoint_solve.py
+++ pyscope/utils/pinpoint_solve.py
@@ -5,7 +5,18 @@ import time
 import click
 from win32com.client import Dispatch
 
+# Remove default stream handler
+logging.getLogger().handlers = []
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
+logger.propagate = False
+
+
+def setup_logger():
+    # Add a StreamHandler to log to the console
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
 
 
 def save_image(filepath):
@@ -19,6 +30,7 @@ def open_image(filepath):
 def platesolve_image(filepath, new_filepath):
     open_image(filepath)
     maxim.PinPointSolve()
+    logger.info(f"Attempting to solve {filepath}")
     try:
         while maxim.PinPointStatus == 3:
             time.sleep(0.1)
@@ -34,7 +46,8 @@ def platesolve_image(filepath, new_filepath):
 
 
 @click.command()
-@click.argument(
+@click.option(
+    "-i",
     "input_dir",
     type=click.Path(exists=True),
     help="""Directory containing images to solve.""",
@@ -48,8 +61,14 @@ def platesolve_image(filepath, new_filepath):
     help="""Directory to save solved images to. If not specified, solved images will be saved to the same directory as the input images.""",
 )
 def pinpoint_solve(input_dir, output_dir=None):
+    # Add input_dir to the end of the current working directory if it is not an absolute path
+    if not os.path.isabs(input_dir):
+        input_dir = os.path.join(os.getcwd(), input_dir)
     if output_dir is None:
         output_dir = input_dir
+    else:
+        if not os.path.isabs(output_dir):
+            output_dir = os.path.join(os.getcwd(), output_dir)
     day_images = os.listdir(input_dir)
     day_filepaths = [os.path.join(input_dir, filename) for filename in day_images]
     new_filepaths = [os.path.join(output_dir, filename) for filename in day_images]
@@ -66,4 +85,5 @@ def pinpoint_solve(input_dir, output_dir=None):
 
 
 if __name__ == "__main__":
+    setup_logger()
     pinpoint_solve()
