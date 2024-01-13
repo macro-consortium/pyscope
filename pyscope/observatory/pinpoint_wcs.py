ad41c913a27d58f45f0059e582107522f4a5a691
pgriffin17 <31374077+pgriffin17@users.noreply.github.com>
2024-01-13 14:54:06 -0600

    Verified functionality of pinpoint_wcs

--- pyscope/observatory/pinpoint_wcs.py
+++ pyscope/observatory/pinpoint_wcs.py
@@ -2,6 +2,9 @@ import logging
 import platform
 import time
 
+import astropy.coordinates as coord
+import astropy.io.fits as pyfits
+
 logger = logging.getLogger(__name__)
 
 from ..utils import _force_async
@@ -29,7 +32,7 @@ class PinpointWCS(WCS):
         ra_dec_units=("hour", "deg"),
         solve_timeout=60,
         catalog=3,
-        catalog_path="C:\GSC-1.1",
+        catalog_path="C:\\GSC11",
     ):
         logger.debug(
             f"""PinpointWCS.Solve(
@@ -40,13 +43,15 @@ class PinpointWCS(WCS):
 
         self._solver.AttachFITS(filepath)
 
-        if kwargs.get("ra", None) is None or kwargs.get("dec", None) is None:
+        if ra is None or dec is None:
             with pyfits.open(filepath) as hdul:
                 ra = hdul[0].header[ra_key]
                 dec = hdul[0].header[dec_key]
         else:
-            ra = kwargs.get("ra", self._solver.TargetRightAscension)
-            dec = kwargs.get("dec", self._solver.TargetDeclination)
+            ra = self._solver.TargetRightAscension
+            dec = self._solver.TargetDeclination
+
+        print(f"RA: {ra}, DEC: {dec}")
 
         obj = coord.SkyCoord(ra, dec, unit=ra_dec_units, frame="icrs")
         self._solver.RightAscension = obj.ra.hour
@@ -54,11 +59,12 @@ class PinpointWCS(WCS):
 
         self._solver.ArcsecPerPixelHoriz = scale_est
         self._solver.ArcsecPerPixelVert = scale_est
-        self.Catalog = catalog
-        self.CatalogPath = catalog_path
+        self._solver.Catalog = catalog
+        self._solver.CatalogPath = catalog_path
 
         solved = None
-        solved = self._async_solve()
+        # solved = self._async_solve()
+        solved = self._solver.Solve()
         start_solve = time.time()
         while time.time() - start_solve < solve_timeout and solved is None:
             time.sleep(1)
@@ -66,7 +72,10 @@ class PinpointWCS(WCS):
             if solved is None:
                 raise Exception("Pinpoint solve timed out.")
             else:
-                return solved
+                print("Solved!")
+                print(f"RA: {self._solver.RightAscension}")
+                print(f"DEC: {self._solver.Declination}")
+                # return solved
 
         self._solver.UpdateFITS()
         self._solver.DetachFITS()
@@ -74,4 +83,5 @@ class PinpointWCS(WCS):
 
     @_force_async
     def _async_solve(self):
-        self.Solver.Solve()
+        # Currently doesn't work properly. Needed to synchronously solve.
+        self._solver.Solve()
