668bf0907a16832d7b55db88229ba0855926d601
WGolay <wgolay30@gmail.com>
2024-04-02 00:21:51 -0700

    Formatting patch

--- pyscope/reduction/avg_fits_ccdproc.py
+++ pyscope/reduction/avg_fits_ccdproc.py
@@ -1,32 +1,33 @@
 import logging
+import os
 
+import astropy.units as u
+import ccdproc
 import click
 import numpy as np
 from astropy.io import fits
-import astropy.units as u
-import ccdproc
 from astropy.nddata import CCDData
-import os
 
 logger = logging.getLogger(__name__)
 
 """
 TODO: use ccdproc to average FITS files
 
-for reading in files as CCDData types: 
+for reading in files as CCDData types:
     default to user specified, then header BUNIT, then default to adu
 
     if unit == None:
         then the user didn't specify a unit, so check the header for BUNIT
-        if BUNIT exists, use that, 
+        if BUNIT exists, use that,
             unit = header["BUNIT"]
         else:
             unit = "adu"
-    
-            
+
+
 
     or if the header doesn't contain BUNIT, set BUNIT to adu, then use that
 """
+
 
 @click.command(
     epilog="""Check out the documentation at
@@ -52,15 +53,23 @@ for reading in files as CCDData types:
 @click.option(
     "-d",
     "--datatype",
-    type=click.Choice([
-        np.int_, np.int8, np.int16, 
-        np.int32, np.int64,
-        
-        np.uint, np.uint8, np.uint16, 
-        np.uint32, np.uint64,
-        
-        np.float_, np.float32, np.float64
-    ]),
+    type=click.Choice(
+        [
+            np.int_,
+            np.int8,
+            np.int16,
+            np.int32,
+            np.int64,
+            np.uint,
+            np.uint8,
+            np.uint16,
+            np.uint32,
+            np.uint64,
+            np.float_,
+            np.float32,
+            np.float64,
+        ]
+    ),
     default=np.uint16,
     show_choices=True,
     show_default=True,
@@ -214,28 +223,29 @@ for reading in files as CCDData types:
 @click.argument("fnames", nargs=-1, type=click.Path(exists=True, resolve_path=True))
 @click.version_option()
 def avg_fits_ccdproc_cli(
-    mode, 
-    outfile, 
-    fnames, 
+    mode,
+    outfile,
+    fnames,
     datatype=np.uint16,
-    weights=None, 
-    scale=None, 
-    mem_limit=16000000000.0, 
-    clip_extrema=False, 
-    nlow=1, 
-    nhigh=1, 
-    minmax_clip=False, 
-    minmax_clip_min=None, 
-    minmax_clip_max=None, 
-    sigma_clip=False, 
-    sigma_clip_low_thresh=3, 
-    sigma_clip_high_thresh=3, 
-    sigma_clip_func=None, 
-    sigma_clip_dev_func=None, 
-    combine_uncertainty_function=None, 
+    weights=None,
+    scale=None,
+    mem_limit=16000000000.0,
+    clip_extrema=False,
+    nlow=1,
+    nhigh=1,
+    minmax_clip=False,
+    minmax_clip_min=None,
+    minmax_clip_max=None,
+    sigma_clip=False,
+    sigma_clip_low_thresh=3,
+    sigma_clip_high_thresh=3,
+    sigma_clip_func=None,
+    sigma_clip_dev_func=None,
+    combine_uncertainty_function=None,
     overwrite_output=False,
-    unit='adu', 
-    verbose=False):
+    unit="adu",
+    verbose=False,
+):
     """Advanced version of avg_fits. Averages fits files using ccdproc.combine method.
 
     Parameters
@@ -245,68 +255,68 @@ def avg_fits_ccdproc_cli(
 
     outfile : str
         path to save averaged FITS file.
-    
+
     fnames : list
         list of FITS file paths to average.
-    
+
     datatype : np.datatype, optional
         intermediate and resulting dtype for combined CCDs, by default np.uint16
-    
+
     weights : np.ndarray, optional
         Weights to be used when combining images. An array with the weight values. The dimensions should match the the dimensions of the data arrays being combined., by default None
-    
+
     scale : callable or np.ndarray, optional
         Scaling factor to be used when combining images. Images are multiplied by scaling prior to combining them. Scaling may be either a function, which will be applied to each image to determine the scaling factor, or a list or array whose length is the number of images in the Combiner, by default None
-    
+
     mem_limit : float, optional
         Maximum memory which should be used while combining (in bytes), by default 16000000000.0
-    
+
     clip_extrema : bool, optional
         Set to True if you want to mask pixels using an IRAF-like minmax clipping algorithm. The algorithm will mask the lowest nlow values and the highest nhigh values before combining the values to make up a single pixel in the resulting image. For example, the image will be a combination of Nimages-low-nhigh pixel values instead of the combination of Nimages. Parameters below are valid only when clip_extrema is set to True, by default False
-    
+
     nlow : int, optional
         Number of low values to reject from the combination, by default 1
-    
+
     nhigh : int, optional
         Number of high values to reject from the combination, by default 1
-    
+
     minmax_clip : bool, optional
         Set to True if you want to mask all pixels that are below minmax_clip_min or above minmax_clip_max before combining, by default False
-    
+
     minmax_clip_min : float, optional
         All pixels with values below min_clip will be masked, by default None
-    
+
     minmax_clip_max : flaot, optional
         All pixels with values above min_clip will be masked, by default None
-    
+
     sigma_clip : bool, optional
         Set to True if you want to reject pixels which have deviations greater than those set by the threshold values. The algorithm will first calculated a baseline value using the function specified in func and deviation based on sigma_clip_dev_func and the input data array. Any pixel with a deviation from the baseline value greater than that set by sigma_clip_high_thresh or lower than that set by sigma_clip_low_thresh will be rejected, by default False
-    
+
     sigma_clip_low_thresh : int, optional
         Threshold for rejecting pixels that deviate below the baseline value. If negative value, then will be convert to a positive value. If None, no rejection will be done based on low_thresh, by default 3
-    
+
     sigma_clip_high_thresh : int, optional
         Threshold for rejecting pixels that deviate above the baseline value. If None, no rejection will be done based on high_thresh, by default 3
-    
+
     sigma_clip_func : callable, optional
         The statistic or callable function/object used to compute the center value for the clipping. If using a callable function/object and the axis keyword is used, then it must be able to ignore NaNs (e.g., numpy.nanmean) and it must have an axis keyword to return an array with axis dimension(s) removed. The default is 'median', by default None
-    
+
     sigma_clip_dev_func : callable, optional
         The statistic or callable function/object used to compute the standard deviation about the center value. If using a callable function/object and the axis keyword is used, then it must be able to ignore NaNs (e.g., numpy.nanstd) and it must have an axis keyword to return an array with axis dimension(s) removed. The default is 'std', by default None
-    
+
     combine_uncertainty_function : callable, optional
         If None use the default uncertainty func when using average, median or sum combine, otherwise use the function provided, by default None
-    
+
     overwrite_output : bool, optional
         If output_file is specified, this is passed to the astropy.nddata.fits_ccddata_writer under the keyword overwrite; has no effect otherwise., by default False
-    
+
     unit : str, optional
         unit for CCDData objects, by default 'adu'
-    
+
     verbose : bool, optional
         verbosity of logger, by default False
     """
-    
+
     if verbose:
         logger.setLevel(logging.DEBUG)
 
@@ -321,10 +331,9 @@ def avg_fits_ccdproc_cli(
     logger.info("Loading FITS files...")
 
     images = np.array([fits.open(fname)[0].data for fname in fnames])
-    
-    
+
     images = images.astype(datatype)
-    
+
     logger.info(f"Loaded {len(images)} FITS files")
 
     logger.info("Averaging FITS files...")
@@ -332,64 +341,62 @@ def avg_fits_ccdproc_cli(
     if str(mode) == "0":
         logger.debug("Calculating median...")
         image_avg = ccdproc.combine(
-            img_list=fnames, 
-            method='median', 
-            dtype=datatype, 
-            weights=weights, 
-            scale=scale, 
-            mem_limit=mem_limit, 
-            clip_extrema=clip_extrema, 
-            nlow=nlow, 
-            nhigh=nhigh, 
-            minmax_clip=minmax_clip, 
-            minmax_clip_min=minmax_clip_min, 
-            minmax_clip_max=minmax_clip_max, 
-            sigma_clip=sigma_clip, 
-            sigma_clip_low_thresh=sigma_clip_low_thresh, 
-            sigma_clip_high_thresh=sigma_clip_high_thresh, 
-            sigma_clip_func=sigma_clip_func, 
-            sigma_clip_dev_func=sigma_clip_dev_func, 
-            combine_uncertainty_function=combine_uncertainty_function, 
+            img_list=fnames,
+            method="median",
+            dtype=datatype,
+            weights=weights,
+            scale=scale,
+            mem_limit=mem_limit,
+            clip_extrema=clip_extrema,
+            nlow=nlow,
+            nhigh=nhigh,
+            minmax_clip=minmax_clip,
+            minmax_clip_min=minmax_clip_min,
+            minmax_clip_max=minmax_clip_max,
+            sigma_clip=sigma_clip,
+            sigma_clip_low_thresh=sigma_clip_low_thresh,
+            sigma_clip_high_thresh=sigma_clip_high_thresh,
+            sigma_clip_func=sigma_clip_func,
+            sigma_clip_dev_func=sigma_clip_dev_func,
+            combine_uncertainty_function=combine_uncertainty_function,
             overwrite_output=overwrite_output,
-            format='fits', 
-            unit=unit)
+            format="fits",
+            unit=unit,
+        )
     elif str(mode) == "1":
         logger.debug("Calculating mean...")
         image_avg = ccdproc.combine(
-            fnames, 
-            method='average', 
-            dtype=datatype, 
-            weights=weights, 
-            scale=scale, 
-            mem_limit=mem_limit, 
-            clip_extrema=clip_extrema, 
-            nlow=nlow, 
-            nhigh=nhigh, 
-            minmax_clip=minmax_clip, 
-            minmax_clip_min=minmax_clip_min, 
-            minmax_clip_max=minmax_clip_max, 
-            sigma_clip=sigma_clip, 
-            sigma_clip_low_thresh=sigma_clip_low_thresh, 
-            sigma_clip_high_thresh=sigma_clip_high_thresh, 
-            sigma_clip_func=sigma_clip_func, 
-            sigma_clip_dev_func=sigma_clip_dev_func, 
-            combine_uncertainty_function=combine_uncertainty_function, 
+            fnames,
+            method="average",
+            dtype=datatype,
+            weights=weights,
+            scale=scale,
+            mem_limit=mem_limit,
+            clip_extrema=clip_extrema,
+            nlow=nlow,
+            nhigh=nhigh,
+            minmax_clip=minmax_clip,
+            minmax_clip_min=minmax_clip_min,
+            minmax_clip_max=minmax_clip_max,
+            sigma_clip=sigma_clip,
+            sigma_clip_low_thresh=sigma_clip_low_thresh,
+            sigma_clip_high_thresh=sigma_clip_high_thresh,
+            sigma_clip_func=sigma_clip_func,
+            sigma_clip_dev_func=sigma_clip_dev_func,
+            combine_uncertainty_function=combine_uncertainty_function,
             overwrite_output=overwrite_output,
-            format='fits', 
-            unit=unit)
-
+            format="fits",
+            unit=unit,
+        )
 
     logger.debug(f"Image mean: {np.mean(image_avg.data)}")
     logger.debug(f"Image median: {np.median(image_avg.data)}")
 
-
     logger.info(f"Saving averaged FITS file to {outfile}")
-
 
     with fits.open(fnames[-1]) as hdul:
         hdr = hdul[0].header
 
-    
     hdr.add_comment(f"Averaged {len(images)} FITS files using pyscope")
     hdr.add_comment(f"Average mode: {mode}")
     hdr.add_comment(f"Unit: {unit}")
@@ -414,8 +421,6 @@ def avg_fits_ccdproc_cli(
     fits.writeto(outfile, image_avg, hdr, overwrite=True)
 
     logger.info("Done!")
-
-
 
 
 avg_fits_ccdproc = avg_fits_ccdproc_cli.callback
--- tests/reduction/test_avg_fits.py
+++ tests/reduction/test_avg_fits.py
@@ -1,10 +1,12 @@
-import pytest
-import numpy as np
-from astropy.io import fits
-from pyscope.reduction import avg_fits
 import glob
 import os
 import shutil
+
+import numpy as np
+import pytest
+from astropy.io import fits
+
+from pyscope.reduction import avg_fits
 
 
 def test_avg_fits(tmp_path):
@@ -13,40 +15,56 @@ def test_avg_fits(tmp_path):
     """
     cwd = os.getcwd()
 
-    if os.path.exists(os.path.join(cwd, tmp_path, 'averaged')):
-        shutil.rmtree(os.path.join(cwd, tmp_path, 'averaged'))
-    os.makedirs(os.path.join(cwd, tmp_path, 'averaged'))
-    # create a test FITS file of 
-    for i in range(1,11):
-        data = np.ones((10,10), dtype=np.uint16)
+    if os.path.exists(os.path.join(cwd, tmp_path, "averaged")):
+        shutil.rmtree(os.path.join(cwd, tmp_path, "averaged"))
+    os.makedirs(os.path.join(cwd, tmp_path, "averaged"))
+    # create a test FITS file of
+    for i in range(1, 11):
+        data = np.ones((10, 10), dtype=np.uint16)
         fits.writeto(f"{tmp_path}/avg_fits_test_{i}.fits", data * i, overwrite=True)
         print(data * i)
 
     files = glob.glob(f"{tmp_path}/avg_fits_test_*.fits")
-    
+
     # test median mode
-    avg_fits(mode="0", outfile=f"{tmp_path}/averaged/avg_fits_median.fits", fnames=files, verbose=True)
+    avg_fits(
+        mode="0",
+        outfile=f"{tmp_path}/averaged/avg_fits_median.fits",
+        fnames=files,
+        verbose=True,
+    )
     med_data = fits.getdata(f"{tmp_path}/averaged/avg_fits_median.fits")
-    med = np.ones((10,10), dtype=np.uint16)
-    med = med * np.median(range(1,11)).astype(np.uint16)
+    med = np.ones((10, 10), dtype=np.uint16)
+    med = med * np.median(range(1, 11)).astype(np.uint16)
     print(med_data, med)
     assert np.array_equal(med_data, med)
     print("correctly calculated median")
-    
+
     # test mean mode
-    avg_fits(mode="1", outfile=f"{tmp_path}/averaged/avg_fits_mean.fits", fnames=files, verbose=True)
+    avg_fits(
+        mode="1",
+        outfile=f"{tmp_path}/averaged/avg_fits_mean.fits",
+        fnames=files,
+        verbose=True,
+    )
     mean_data = fits.getdata(f"{tmp_path}/averaged/avg_fits_mean.fits")
-    mean = np.ones((10,10), dtype=np.uint16)
-    mean = mean * np.mean(range(1,11)).astype(np.uint16)
+    mean = np.ones((10, 10), dtype=np.uint16)
+    mean = mean * np.mean(range(1, 11)).astype(np.uint16)
     print(mean_data, mean)
     assert np.array_equal(mean_data, mean)
     print("correctly calculated mean")
 
     # test different data type
-    avg_fits(mode="1", outfile=f"{tmp_path}/averaged/avg_fits_mean.fits", fnames=files, datatype=np.float64, verbose=True)
+    avg_fits(
+        mode="1",
+        outfile=f"{tmp_path}/averaged/avg_fits_mean.fits",
+        fnames=files,
+        datatype=np.float64,
+        verbose=True,
+    )
     mean_dataf = fits.getdata(f"{tmp_path}/averaged/avg_fits_mean.fits")
-    meanf = np.ones((10,10), dtype=np.float64)
-    meanf = meanf * np.mean(range(1,11)).astype(np.float64)
+    meanf = np.ones((10, 10), dtype=np.float64)
+    meanf = meanf * np.mean(range(1, 11)).astype(np.float64)
     assert np.array_equal(mean_dataf, meanf)
     print(mean_dataf, meanf)
     print("correctly calculated mean with different data type")
