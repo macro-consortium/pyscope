713a2ae552903ecde08a0c0fdf7f0de6a526831c
wastjohn <st.john.will@gmail.com>
2024-04-12 10:41:39 -0500

    fixed avg-fits click functionality in terminal

--- tests/reduction/test_avg_fits.py
+++ tests/reduction/test_avg_fits.py
@@ -59,7 +59,7 @@ def test_avg_fits(tmp_path):
         mode="1",
         outfile=f"{tmp_path}/averaged/avg_fits_mean.fits",
         fnames=files,
-        datatype=np.float64,
+        datatype="float64",
         verbose=True,
     )
     mean_dataf = fits.getdata(f"{tmp_path}/averaged/avg_fits_mean.fits")
