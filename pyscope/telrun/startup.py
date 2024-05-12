be38baa2d118efc4b1dd0c389a33451d66b6bbbe
WGolay <wgolay30@gmail.com>
2024-05-12 15:17:12 -0400

    Remove syncing tools

--- pyscope/telrun/startup.py
+++ pyscope/telrun/startup.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import click
 
-from .synctools import sync_manager
 from .telrun_operator import TelrunOperator
 
 
@@ -25,29 +24,4 @@ def start_telrun_operator_cli(path="./", gui=True):
     telrun.mainloop()
 
 
-@click.command(
-    epilog="""Check out the documentation at
-               https://pyscope.readthedocs.io/en/latest/
-               for more information."""
-)
-@click.option(
-    "-a",
-    "--do-async",
-    is_flag=True,
-    default=False,
-    show_default=True,
-    help="Run the sync manager in async mode",
-)
-@click.argument(
-    "path", type=click.Path(resolve_path=True), default="./", required=False
-)
-@click.version_option()
-def start_sync_manager_cli(path="./", do_async=False):
-    logger = logging.getLogger("pyscope")
-    logging.basicConfig(level=logging.INFO)
-
-    sync_manager(config=Path(path) / "config/sync.cfg", do_async=do_async)
-
-
 start_telrun_operator = start_telrun_operator_cli.callback
-start_sync_manager = start_sync_manager_cli.callback
